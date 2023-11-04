4f9de80b3ec0bee04c6402a6306becd0345528a3
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-04 20:40:37 +0545

    refactor: restructure script outside src

--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -1,16 +1,18 @@
-const fs = require('fs');
+import { readFileSync, writeFileSync } from 'fs';
 const version = process.env.VERSION ?? 'none';
 
 if (version !== 'none') {
   try {
-    const packageJsonString = fs.readFileSync('package.json', 'utf-8');
+    const packageJsonString = readFileSync('package.json', 'utf-8');
     const packageJson = JSON.parse(packageJsonString);
     packageJson.version = version;
 
     const updatedPackageJsonString = JSON.stringify(packageJson, null, 2);
-    fs.writeFileSync('package.json', updatedPackageJsonString, 'utf-8');
+    writeFileSync('package.json', updatedPackageJsonString, 'utf-8');
     console.log('Version updated ✅');
   } catch (error) {
     console.log(error);
   }
 }
+
+export {};
