e55e0ea22f7eb52e0cc5cbd5442c9fa646d763f1
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-04 21:32:48 +0545

    feat: use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import helmet from 'helmet';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import helmet from '@fastify/helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(helmet());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { cors: true },
+  );
 
+  await app.register(helmet);
   // Starts listening for shutdown hooks
   app.enableShutdownHooks();
-
   await app.listen(3000);
 }
 bootstrap();
